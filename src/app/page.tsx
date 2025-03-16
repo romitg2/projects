c93455ca17abba17ac7b9415612ecd0361e6a6fb
romit <romitgabani1@gmail.com>
2025-03-16 15:18:42 +0530

    configured shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div> working </div> 
+    <div>
+      <Button>Click me</Button>
+    </div> 
   );
 }
